43279b86662b7cf19cb5ee1b921a66a04dfd8837
wiktorpp <grazynapp65@gmail.com>
2021-12-26 17:36:13 +0100

    Commit

--- image_to_braille.py
+++ image_to_braille.py
@@ -20,7 +20,7 @@ while True:
 
 
 '''
-
+"""
 def print_braille(array):
     for i in range(0, len(out1), 2):
         for j in range(0, len(out1[0]), 5):
@@ -34,3 +34,18 @@ test=[
 ]
 
 print(print_braille(test))
+"""
+
+#111000101010000010000000
+#111000101010001110111111
+#               100000000
+char = 0b111000101010000010000000
+char = (char & ~0b1) | 0b1 if True else char
+char = (char & ~0b1000) | 0b1000 if True else char
+char = (char & ~0b10) | 0b10 if True else char
+char = (char & ~0b10000) | 0b10000 if True else char
+char = (char & ~0b100) | 0b100 if True else char
+char = (char & ~0b100000) | 0b100000 if True else char
+char = (char & ~0b100000000) | 0b100000000 if True else char
+char = (char & ~0b1000000000) | 0b1000000000 if True else char
+print(char.to_bytes(24, "big").decode('utf-8'))
